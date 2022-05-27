9013c9ecf778f616a099fd359a866f17b1667079
OussElMa <106403046+OussElMa@users.noreply.github.com>
2022-05-27 20:36:44 +0200

    Add files via upload

--- prog.c
+++ prog.c
@@ -2,10 +2,11 @@
 
 int main()
 {
-    int naissance, age;
-    printf("Veuillez indiquer votre année de naissance \n");
-    scanf("%d", &naissance);
-    age = 2022 - naissance;
-    printf("Vous avez %d ans", age);
+    float rayon, surface;
+    printf("Veuillez entrer la valeur du rayon \n");
+    scanf("%f", &rayon);
+    surface = rayon * rayon *3,14;
+    printf("Le rayon est de %.2f, pour calculer la surface nous allons mutliplier rayon par rayon par Pi\n", rayon);
+    printf("La surface est de %f", surface);
     return 0;
 }
