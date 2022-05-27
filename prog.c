278b648c09c9ca3663e8d49462d4538e71d08a52
OussElMa <106403046+OussElMa@users.noreply.github.com>
2022-05-27 20:33:07 +0200

    Add files via upload
    Dans ce programme je calcul l'âge de l'utilisateur.

--- prog.c
+++ prog.c
@@ -2,11 +2,10 @@
 
 int main()
 {
-    float rayon, surface;
-    printf("Veuillez entrer la valeur du rayon \n");
-    scanf("%f", &rayon);
-    surface = rayon * rayon *3,14;
-    printf("Le rayon est de %.2f, pour calculer la surface nous allons mutliplier rayon par rayon par Pi\n", rayon);
-    printf("La surface est de %f", surface);
+    int naissance, age;
+    printf("Veuillez indiquer votre année de naissance \n");
+    scanf("%d", &naissance);
+    age = 2022 - naissance;
+    printf("Vous avez %d ans", age);
     return 0;
 }
